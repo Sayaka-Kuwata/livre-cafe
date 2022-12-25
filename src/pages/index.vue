177b886c1e0420791e1d6809715f545c653453f3
Sayaka-Kuwata <syk.a8785@gmail.com>
2022-12-25 13:12:37 +0900

    Conceptセクション作成

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,18 +2,21 @@
   <div>
     <Header />
     <MainVisual />
+    <Concept />
   </div>
 </template>
 
 <script>
 import Header from "@/organisms/header";
 import MainVisual from "@/organisms/mainvisual";
+import Concept from "@/organisms/concept";
 
 export default {
   name: "IndexPage",
   components: {
     Header,
     MainVisual,
+    Concept,
   },
 };
 </script>

538ba9a87d6b07d0d166e40fd7dbfbf44d2eb978
Sayaka-Kuwata <syk.a8785@gmail.com>
2022-12-27 18:10:53 +0900

    PC作成

---
+++ src/molecules/news-card.vue
@@ -0,0 +1,53 @@
+<template>
+  <ul class="news-card c-flex">
+    <li class="column--3" v-for="card in cards" :key="cards.id">
+      <img :src="card.image" alt="" />
+      <p>
+        <time>{{ card.time }}</time>
+      </p>
+      <p>{{ card.text }}</p>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: "NewsCard",
+  props: ["cards"],
+};
+</script>
+
+<style lang="scss">
+@use "/src/assets/scss/global" as global;
+@use "/src/assets/scss/foundation/variable" as var;
+
+ul {
+  flex-wrap: wrap;
+  justify-content: center;
+  gap: 60px 20px;
+}
+
+.column--3 {
+  width: 30%;
+}
+
+.news-card {
+  img {
+    margin-bottom: 16px;
+    border: 1px solid var.$color-gray;
+    border-top: 4px solid var.$color-primary;
+  }
+  time {
+    display: block;
+    @include global.font-18-bold;
+    letter-spacing: 0.03rem;
+    margin-bottom: 4px;
+  }
+  p {
+    @include global.font-16-normal(var.$color-black);
+    letter-spacing: 0.05rem;
+    line-height: 1.3;
+    white-space: pre-wrap;
+  }
+}
+</style>
